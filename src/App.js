e7462837c31149b2eb3d4cea990479c492041404
Mzynga.eth <mikemwambia@gmail.com>
2021-12-15 23:33:31 +0300

    Updated React home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is a simple react application hosted on github pages.Using a deploy script.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Still learning React
         </a>
       </header>
     </div>
